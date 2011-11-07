4bbab275696fbea9cfb2e02def6441ac00b0211d
Ben Shapiro <github@catchall.getdown.org>
2011-11-07 09:14:01 -0600

    empty project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BigworldProto::Application.config.secret_token = '1cf0f06de3d63138859371563b2e9f014bdc77bcd7f63715447d3c01fba6a041ae8dea64c23f405d8ae33a26346266e4bbeefc3cde3c151cd6c1c05d0ad554fa'
